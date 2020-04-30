d98fcca0858e29e46365c077bddd09b56d04185a
91900 <91900@DESKTOP-VC6QHVC>
2020-04-30 20:39:47 +0530

    Hello Baby...!!!! This is first my commit...

---
+++ MyProjectExplorer/src/com/mytesting/DefaultClassTEst.java
@@ -0,0 +1,11 @@
+package com.mytesting;
+
+public class DefaultClassTEst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hey Baby ... I'm here... !!!");
+	}
+
+}
